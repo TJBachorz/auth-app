94e6851ece1bb06984b6766c7a9a098797d117e7
TJBachorz <TJBachorz@gmail.com>
2020-09-22 15:54:00 -0600

    auth pt 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
             username: params[:username],
             password: params[:password]
         )
-
-        render json: @user
+        @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+        render json: {user: @user, token: @token}
     end
 
     def login
